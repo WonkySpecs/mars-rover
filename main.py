e256a7d3cb339a6af38fc52d48d8574c47d8c665
Will Taylor <w1ll100@hotmail.co.uk>
2023-03-03 18:46:39 +0000

    Add interactive input

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
+from typing import List
+
 import parser
+import robot
+
+
+def read_input() -> ((int, int), List[robot.Robot]):
+    """Interactively read the grid size and robots to run"""
+    print("Welcome to mars rover. Input 'q' at any time to quit")
+    grid_size = None
+    while not grid_size:
+        first = input(
+            "Enter a grid size (two positive ints separated by whitespace): ")
+        if first == "q":
+            exit(0)
+
+        try:
+            grid_size = parser.parse_grid_size(first)
+        except ValueError as ex:
+            print(ex)
+
+    robots = []
+    while robot_line := input(
+            "Enter a robot, or an empty line to run the simulation: "):
+        if robot_line == "q":
+            exit(0)
+
+        try:
+            r = parser.parse_robot(robot_line)
+            if robot.out_of_bounds(r.pos, grid_size):
+                raise ValueError("Robot would start out of bounds")
+            robots.append(r)
+            print("Robot added")
+        except ValueError as ex:
+            print(ex)
+
+    return grid_size, robots
 
 
 def run():
@@ -13,7 +49,8 @@ def run():
     #     "(1, 0, S) FFRLF"
     # ]
     try:
-        grid_size, robots = parser.parse_lines(input_lines)
+        # grid_size, robots = parser.parse_lines(input_lines)
+        grid_size, robots = read_input()
     except ValueError as ex:
         print(f"Input was invalid: {ex}")
         exit(1)
